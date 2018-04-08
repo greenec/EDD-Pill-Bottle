1ab965076aa96113ec70ed3233020cc7bfbfbd5e
Connor Greene <connor@applicationx.net>
2018-04-08 18:43:49 -0400

    "Enable bluetooth" prompt

--- app/www/js/index.js
+++ app/www/js/index.js
@@ -40,11 +40,13 @@ function lockControlInit() {
 }
 
 function bluetoothInit() {
-	bluetoothSerial.list(function (devices) {
-		for(var i = 0; i < devices.length; i++) {
-			var device = devices[i];
-			$('#device-list').append('<option value="' + device.address + '">' + device.name + '</option>');
-		}
+	bluetoothSerial.enable(function() {
+		bluetoothSerial.list(function (devices) {
+			for(var i = 0; i < devices.length; i++) {
+				var device = devices[i];
+				$('#device-list').append('<option value="' + device.address + '">' + device.name + '</option>');
+			}
+		});
 	});
 
 	// TODO: loading animation and error handling for connecting
